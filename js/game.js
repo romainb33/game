b39b9a88c48c2fa353b3bfe35d6f44c60c5895fd
Romain B <romain.bnd@gmail.com>
2021-02-14 20:08:42 +0100

    control and animation of the circles OK

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 const leftBallOne = document.getElementById('left-1');
 const leftCircle = document.querySelector('.circle-left-container')
+const rightCircle = document.querySelector('.circle-right-container')
+
 
 console.log(leftBallOne.getBoundingClientRect())
 
@@ -14,7 +16,7 @@ console.log(leftCircle)
 document.addEventListener('keydown', function (event) {
     if (event.key === 'q') {
         
-            leftCircle.classList.toggle('reverse')
+        leftCircle.classList.toggle('reverse')
         console.log('q is called')
         console.log(leftCircle)
         console.log(leftBallOne.getBoundingClientRect().left.toFixed(0))
@@ -26,4 +28,13 @@ document.addEventListener('keydown', function (event) {
         //DOUBLE THE NUMBER OF REFERENCE…
 
     }
-})    
+})
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'd') {
+        
+        rightCircle.classList.toggle('reverse')
+        console.log('d is called')
+        console.log(rightCircle)
+
+    }
+}) 
