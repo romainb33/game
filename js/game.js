27dd52a10e644d519c813e0bf11aa7ebea6c020c
Romain B <romain.bnd@gmail.com>
2021-02-15 11:24:42 +0100

    add timer and score point

--- js/game.js
+++ js/game.js
@@ -9,21 +9,19 @@ const rightBallTwo = document.getElementById('right-2');
 
 const lines = document.querySelectorAll('.line')
 
-const score = document.querySelector('#score');
-const time = document.querySelector('#time');
+const points = document.querySelector('.points');
+const timer = document.querySelector('.time');
 
 
 // Method for coordinates
 // console.log(leftBallOne.getBoundingClientRect())
 
-// setInterval(() => {
-//     console.log(+leftBallOne.getBoundingClientRect().left.toFixed(0))
-// }, 500);
-
-// console.log(leftBallOne.getBoundingClientRect().left.toFixed(0))
-
 // create a array to stock each ball created
 let currentBalls = [];
+let score = 0;
+let time = 60;
+let intervalId = 0;
+
 
 // Functions
 function newBalls() {
@@ -36,7 +34,6 @@ function newBalls() {
         // setInterval(() => {
         //     console.log(Number(leftCircle.getBoundingClientRect().x.toFixed(0)) + "-" + Number(leftCircle.getBoundingClientRect().y.toFixed(0)))
         //     console.log(Number(balls.getBoundingClientRect().x.toFixed(0)) + "-" + Number(balls.getBoundingClientRect().y.toFixed(0)))
-    
         // }, 20);
 
         currentBalls.push(balls)
@@ -45,60 +42,71 @@ function newBalls() {
         setInterval(() => {
             currentBalls.forEach(ball => {
                 checkSameCoordinates(leftBallOne, ball)
+                checkSameCoordinates(leftBallTwo, ball)
+                checkSameCoordinates(rightBallOne, ball)
+                checkSameCoordinates(rightBallTwo, ball)
             });
-        }, 10);
+        }, 40);
         
         // Remove ball if it reaches the end of the line /!\/!\/!\ Adjust the duration depending on the animation.
         setTimeout(() => {
-        lines[randomIndex].removeChild(lines[randomIndex].firstChild) }, 20000) 
+        lines[randomIndex].removeChild(lines[randomIndex].firstChild) }, 10000) 
 
-    }, Math.random()*1500);
+    }, 4000);
 }
 
 function checkSameCoordinates(circleBall, lineBall) {
-    if (Number(circleBall.getBoundingClientRect().x.toFixed(0)) == Number(lineBall.getBoundingClientRect().x.toFixed(0))
-    && Number(circleBall.getBoundingClientRect().y.toFixed(0)) == Number(lineBall.getBoundingClientRect().y.toFixed(0))) {
-        console.log("it works")
+    let circleBallX = Number(circleBall.getBoundingClientRect().x.toFixed(0)) + 7 ; // to trigger the center add the radius
+    let circleBallY = Number(circleBall.getBoundingClientRect().y.toFixed(0)) + 7;
+
+    let lineBallX = Number(lineBall.getBoundingClientRect().x.toFixed(0)) + 5;  // to trigger the center add the radius
+    let lineBallY = Number(lineBall.getBoundingClientRect().y.toFixed(0)) + 5;
+    if ((circleBallX === lineBallX + 5 || circleBallX === lineBallX - 5) && (circleBallY === lineBallY + 5 || circleBallY === lineBallY - 5)) {
+        console.log("it works");
         lineBall.remove()
+        score++
+        console.log(score)
+        points.textContent = score;   
     }
 }
-// SOME TEST ABOUT THE TOLERANCE
-// function checkSameCoordinates(circleBall, lineBall) {
-//     if ((Number(circleBall.getBoundingClientRect().top.toFixed(1)) - 0.5 <= Number(lineBall.getBoundingClientRect().top.toFixed(1)) && Number(lineBall.getBoundingClientRect().top.toFixed(1))  >= Number(circleBall.getBoundingClientRect().top.toFixed(1)) + 0.5)
-//     && (Number(circleBall.getBoundingClientRect().left.toFixed(1)) - 0.5 <= Number(lineBall.getBoundingClientRect().left.toFixed(1)) && Number(lineBall.getBoundingClientRect().left.toFixed(1)) >= Number(circleBall.getBoundingClientRect().left.toFixed(1)) + 0.5)) {
-//         console.log("it works")
-//         lineBall.remove()
-//     }
-// }
 
+function startTimer() {
+    intervalId = setInterval(() => {
+      time--;
+      timer.textContent = time;
+    }, 1000);
+
+  }
+  
 newBalls()
-
-
+startTimer()
 
 
 //Event listeners
 
 document.addEventListener('keydown', function (event) {
     if (event.key === 'q') {
-        
         leftCircle.classList.toggle('reverse')
-        // console.log('q is called')
-        console.log(leftCircle)
-        console.log(leftBallOne.getBoundingClientRect().left.toFixed(0))
-
-        setInterval(() => {
-        console.log(+leftBallOne.getBoundingClientRect().left.toFixed(0))
-        }, 4000);
+        // setInterval(() => {
+        // console.log(+leftBallOne.getBoundingClientRect().left.toFixed(0))
+        // }, 4000);
         //DOUBLE THE NUMBER OF REFERENCE…
     }
 })
 document.addEventListener('keydown', function (event) {
-    if (event.key === 'd') {
-        
-        rightCircle.classList.toggle('reverse')
-        // console.log('d is called')
-        console.log(rightCircle)
-
+    if (event.key === 'd') {        
+        leftCircle.classList.toggle('reverse')
     }
 })
 
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'ArrowLeft') {
+        rightCircle.classList.toggle('reverse')
+    }
+})
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'ArrowRight') {        
+        rightCircle.classList.toggle('reverse')
+    }
+})
