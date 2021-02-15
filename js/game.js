fdebc94c78d8a1ba92f6a250ba1b27ca2a626837
Romain B <romain.bnd@gmail.com>
2021-02-15 17:38:18 +0100

    main features and HTML,CSS OK

--- js/game.js
+++ js/game.js
@@ -1,8 +1,11 @@
 // Link
 const playBtn = document.querySelector('#play-btn')
+const playAgain = document.querySelector('#play-again')
+
 const gameIntro = document.querySelector('#intro-wrapper')
-const gameZone = document.querySelector('#game-zone')
+const gameContainer = document.querySelector('#game-container')
 const gameWrapper = document.querySelector('#game-wrapper')
+const scoreWrapper = document.querySelector('#score-wrapper')
 
 const leftCircle = document.querySelector('.circle-left-container')
 const leftBallOne = document.getElementById('left-1');
@@ -15,6 +18,7 @@ const rightBallTwo = document.getElementById('right-2');
 const lines = document.querySelectorAll('.line')
 
 const points = document.querySelector('.points');
+const finalScore = document.querySelector('.final-score');
 const timer = document.querySelector('.time');
 
 
@@ -27,14 +31,14 @@ const timer = document.querySelector('.time');
 let currentBalls = [];
 
 let score = 0;
-let time = 10;
-let gameStarted = false;
-let intervallBalls = 0;
-let intervallTimer = 0;
+let time = 60;
+let intervalId = 0;
+let intervalId2 = 0;
+let intervalTime = 0;
 
 // Functions
 function newBalls() {
-        setInterval(() => {
+        intervalId = setInterval(() => {
             let randomIndex = Math.floor(Math.random()*lines.length);
             const balls = document.createElement('div');
             balls.classList.add('line-ball');
@@ -42,7 +46,7 @@ function newBalls() {
     
             currentBalls.push(balls)        
             // Calculate the realtime position of each balls and circleBalls
-            setInterval(() => {
+            intervalId2 = setInterval(() => {
                 currentBalls.forEach(ball => {
                     checkSameCoordinates(leftBallOne, ball);
                     checkSameCoordinates(leftBallTwo, ball);
@@ -54,7 +58,7 @@ function newBalls() {
             // Remove ball if it reaches the end of the line 
             //!\/!\/!\ Adjust the duration depending on the animation.
             setTimeout(() => {
-            lines[randomIndex].removeChild(lines[randomIndex].firstChild) }, 6000) 
+            lines[randomIndex].removeChild(lines[randomIndex].firstChild) }, 5000) 
         }, 1500);
 }
 
@@ -66,54 +70,50 @@ function checkSameCoordinates(circleBall, lineBall) {
     let lineBallY = Number(lineBall.getBoundingClientRect().y.toFixed(0)) + 5;
     
     // console.log("circleBall= " + circleBallX + " --- " + circleBallY + "lineBall= " + lineBallX + ";" +lineBallY)        
-    if (gameStarted) {
         if ((circleBallX - 6 <= lineBallX && circleBallX + 6 >= lineBallX) && (circleBallY <= lineBallY + 6 && circleBallY >= lineBallY - 6)) {
             lineBall.remove()
             score++
             points.textContent = score;
         }
-    }
-    else {
-        console.log("GAME OVER")
-        return;
-    }
 }
 
 function timeCountdown() {
-        setInterval(() => {
+    intervalTime = setInterval(() => {
             if (time > 0) {
                 timer.textContent = time;
-            time--;
+                time--;
             }
             else {
-                stopTheGame()
-                setTimeout(function() {
-                    gameZone.remove();
-                }, 1000)
+                stopTheGame();
+                addScoreBoard()
             }
         }, 1000)
-    
 }
 
 function startTheGame(){
-    if (gameStarted) {
-        newBalls(gameStarted);
+        newBalls();
         timeCountdown();
-    }
 }
 
 function stopTheGame() {
-    gameStarted = false;
-    newBalls(gameStarted);
     time = 0;
     timer.textContent = time;
+    clearInterval(intervalId);
+    clearInterval(intervalId2);
+    clearInterval(intervalTime);
+   
+    
+}
+function addScoreBoard() {
+    gameContainer.remove();
+    scoreWrapper.style.display = "flex";
+    finalScore.textContent = score;
 }
 
 
 //Event listeners
 playBtn.addEventListener('click', function() {
     console.log("hello")
-    gameStarted = true;
     setTimeout(startTheGame, 1000);
     
     gameIntro.style.display = "none"
@@ -124,10 +124,6 @@ playBtn.addEventListener('click', function() {
 document.addEventListener('keydown', function (event) {
     if (event.key === 'q') {
         leftCircle.classList.toggle('reverse')
-        // setInterval(() => {
-        // console.log(+leftBallOne.getBoundingClientRect().left.toFixed(0))
-        // }, 4000);
-        //DOUBLE THE NUMBER OF REFERENCE…
     }
 })
 document.addEventListener('keydown', function (event) {
@@ -145,4 +141,8 @@ document.addEventListener('keydown', function (event) {
     if (event.key === 'ArrowRight') {        
         rightCircle.classList.toggle('reverse')
     }
+})
+
+playAgain.addEventListener("click", function() {
+    document.location.reload()
 })
