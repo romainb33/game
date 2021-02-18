a904a9c178880e858f51d55096bdc20e34b7b04e
Romain B <romain.bnd@gmail.com>
2021-02-18 11:10:58 +0100

    refacto and optimization

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-// Links
+// LINKS
 const playBtn = document.querySelector("#play-btn");
 const playAgain = document.querySelector("#play-again");
 
@@ -22,10 +22,10 @@ const points = document.querySelector(".points");
 const finalScore = document.querySelector(".final-score");
 const timer = document.querySelector(".time");
 
-// Sounds
-const audio = new Audio('../game/sounds/cello1.mp3');
+// SOUNDS
+const audio = new Audio("./sounds/cello1.mp3");
 audio.volume = 0.6;
-const drop = new Audio('../game/sounds/drop.mp3');
+const drop = new Audio("./sounds/drop.mp3");
 drop.volume = 0.4;
 
 // create a array to stock each ball created
@@ -37,10 +37,11 @@ let intervalId = 0;
 let intervalId2 = 0;
 let intervalTime = 0;
 
-let i = 0
+let i = 0;
 let ballIndex = "ball" + i;
 
-// Functions
+// FUNCTIONS
+// Create Balls on horizontal lines and remove it at the end.
 function newBalls() {
   intervalId = setInterval(() => {
     ballIndex = "ball" + i++;
@@ -48,13 +49,7 @@ function newBalls() {
     const balls = document.createElement("div");
     balls.setAttribute("id", ballIndex);
     balls.classList.add("line-ball");
-    let randomColor = Math.floor(Math.random() * 2);
-    if (randomColor === 1) {
-      balls.classList.add("yellow");
-    } else {
-      balls.classList.add("blue");
-    }
-    
+    RandomColors(balls);
     lines[randomIndex].appendChild(balls);
 
     currentBalls.push(balls);
@@ -68,7 +63,7 @@ function newBalls() {
       });
     }, 40);
     let element = document.getElementById(ballIndex);
-    
+
     // Remove ball if it reaches the end of the line
     //!\/!\/!\ Adjust the duration depending on the animation.
     setTimeout(() => {
@@ -77,6 +72,17 @@ function newBalls() {
   }, 1000);
 }
 
+// Add a random class
+function RandomColors(ball) {
+  let randomColor = Math.floor(Math.random() * 2);
+  if (randomColor === 1) {
+    ball.classList.add("yellow");
+  } else {
+    ball.classList.add("blue");
+  }
+}
+
+// Check if big and small balls are in the same area (add a tolerance of 12px (-6/+6))
 function checkSameCoordinates(circleBall, lineBall) {
   let circleBallX = Number(circleBall.getBoundingClientRect().x.toFixed(0)) + 7; // to trigger the center add the radius
   let circleBallY = Number(circleBall.getBoundingClientRect().y.toFixed(0)) + 7;
@@ -85,74 +91,79 @@ function checkSameCoordinates(circleBall, lineBall) {
   let lineBallY = Number(lineBall.getBoundingClientRect().y.toFixed(0)) + 5;
 
   if (
-    circleBallX - 6 <= lineBallX && circleBallX + 6 >= lineBallX &&
-    circleBallY <= lineBallY + 6 && circleBallY >= lineBallY - 6) {
-    return true;
-  }
-  else {
-    return false;
-  }
-}
-
-function checkColor(circleBall, lineBall) {
-  if ( (circleBall.classList.contains('blue') && lineBall.classList.contains('blue')) 
-    || (circleBall.classList.contains('yellow') && lineBall.classList.contains('yellow'))){
+    circleBallX - 6 <= lineBallX &&
+    circleBallX + 6 >= lineBallX &&
+    circleBallY <= lineBallY + 6 &&
+    circleBallY >= lineBallY - 6
+  ) {
     return true;
   } else {
     return false;
   }
 }
 
-function addPoints(circleBall, lineBall) {
-  if (checkColor(circleBall, lineBall) && checkSameCoordinates(circleBall, lineBall)) {
-    addHalo(circleBall);
-    infoPoint(circleBall,lineBall) 
-    drop.play();
-    lineBall.remove();
-    // currentBalls.slice(lineBall, 1) // clean the array not sure it works
-    score += 15;
-    points.textContent = score;
-  }
-  else if (!checkColor(circleBall, lineBall) && checkSameCoordinates(circleBall, lineBall)) {
-    addHalo(circleBall)
-    infoPoint(circleBall,lineBall) 
-    drop.play();
-    lineBall.remove();
-    score -= 5;
-    points.textContent = score;
+// Controls the value of the class.
+function checkColor(circleBall, lineBall) {
+  if (
+    (circleBall.classList.contains("blue") &&
+      lineBall.classList.contains("blue")) ||
+    (circleBall.classList.contains("yellow") &&
+      lineBall.classList.contains("yellow"))
+  ) {
+    return true;
+  } else {
+    return false;
   }
 }
 
+// Add animation to the big balls
 function addHalo(circleBall) {
-  let halo = document.createElement('span');
-  halo.classList.add('halo')
+  let halo = document.createElement("span");
+  halo.classList.add("halo");
   circleBall.appendChild(halo);
+  setTimeout(function () {
+    halo.parentNode.removeChild(halo);
+  }, 300);
 }
 
-// Demander ce qui est le mieux entre ces 2 fonctions ou infoPoint()
-// function plusPoint() {
-//   let plus = document.createElement('span');
-//   plus.innerText = "+15";
-//   plus.classList.add('plus-minus');
-//   linesZone.appendChild(plus);
-// }
-// function minusPoint() {
-//   let minus = document.createElement('span');
-//   minus.innerText = "-5";
-//   minus.classList.add('plus-minus');
-//   minus.style.left = "40%";
-//   linesZone.appendChild(minus);
-// }
-
+// Display the info about the points you gain/lose after each balls grabed
 function infoPoint(circleBall, lineBall) {
-  let info = document.createElement('span');
-  info.classList.add('plus-minus');
-  linesZone.appendChild(info);
+  const info = document.querySelector(".plus-minus");
+  info.classList.add("anim-point");
   if (!checkColor(circleBall, lineBall)) {
     info.innerText = "-5";
     info.style.left = "40%";
   } else if (checkColor(circleBall, lineBall)) {
     info.innerText = "+15";
+  }
+  setTimeout(function () {
+    info.classList.remove("anim-point");
+  }, 700);
+}
+// play animations addHalo()-infoPoint(), and remove the element
+function grabBallsAnimation(circleBall, lineBall) {
+  addHalo(circleBall);
+  infoPoint(circleBall, lineBall);
+  drop.play();
+  lineBall.remove();
+}
+
+// Increase/Decrease the score and
+function addPoints(circleBall, lineBall) {
+  if (
+    checkColor(circleBall, lineBall) &&
+    checkSameCoordinates(circleBall, lineBall)
+  ) {
+    grabBallsAnimation(circleBall, lineBall);
+    score += 15;
+    points.textContent = score;
+  } else if (
+    !checkColor(circleBall, lineBall) &&
+    checkSameCoordinates(circleBall, lineBall)
+  ) {
+    grabBallsAnimation(circleBall, lineBall);
+    score -= 5;
+    points.textContent = score;
   }
 }
 
@@ -164,8 +175,8 @@ function timeCountdown() {
     } else {
       stopTheGame();
       addScoreBoard();
-      getHighScore()
-      changeHighScore()
+      getHighScore();
+      changeHighScore();
     }
   }, 1200);
 }
@@ -190,46 +201,44 @@ function addScoreBoard() {
   finalScore.textContent = score;
 }
 
-
 // LOCAL STORAGE
 //set initial value for high score
-const highScore = document.getElementById('high-score')
-console.log(highScore)
+const highScore = document.getElementById("high-score");
+console.log(highScore);
 
 let highestscore = localStorage.getItem("highscore");
 function getHighScore() {
   const newScore = finalScore.textContent;
-  console.log("newScore is called - " +  newScore)
-  if(highestscore !== null){
+  console.log("newScore is called - " + newScore);
+  if (highestscore !== null) {
     if (newScore > Number(highestscore)) {
-        localStorage.setItem("highscore", newScore);
-        highestscore = localStorage.getItem("highscore");
-        console.log("highscore is =" + highestscore)
-        return highestscore
-    }
-  }
-  else{
       localStorage.setItem("highscore", newScore);
       highestscore = localStorage.getItem("highscore");
-      return highestscore
+      console.log("highscore is =" + highestscore);
+      return highestscore;
     }
+  } else {
+    localStorage.setItem("highscore", newScore);
+    highestscore = localStorage.getItem("highscore");
+    return highestscore;
+  }
 }
 function changeHighScore() {
   highScore.textContent = highestscore;
 }
 
-
 // Change rules depending on the media
 const rulesText = document.querySelector("#rules-text");
-const mediaQuery = window.matchMedia('(max-width: 1366px)')
+const mediaQuery = window.matchMedia("(max-width: 1366px)");
 
 if (mediaQuery.matches && navigator.maxTouchPoints > 1) {
-  rulesText.textContent = "Tap on each circle to change their direction and grab a maximum of orbs."
+  rulesText.textContent =
+    "Tap on each circle to change their direction and grab a maximum of orbs.";
 }
 
-//Event listeners
+//EVENT LISTENERS
 playBtn.addEventListener("click", function () {
-  audio.play()
+  audio.play();
   setTimeout(startTheGame, 2500);
 
   gameIntro.style.display = "none";
@@ -268,10 +277,10 @@ playAgain.addEventListener("click", function () {
 // FOR MULTITOUCH DEVICES
 if (navigator.maxTouchPoints > 1) {
   leftCircle.addEventListener("click", function (event) {
-      leftCircle.classList.toggle("reverse");
+    leftCircle.classList.toggle("reverse");
   });
-  
+
   rightCircle.addEventListener("click", function (event) {
-      rightCircle.classList.toggle("reverse");
+    rightCircle.classList.toggle("reverse");
   });
 }
